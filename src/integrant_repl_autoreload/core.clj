43d639a796951373bd12c936ab4360a720acfad7
Tim Greene <tim.g.greene@gmail.com>
2021-02-12 21:43:02 +0000

    Add watch and reset implementation

---
+++ src/integrant_repl_autoreload/core.clj
@@ -0,0 +1,33 @@
+(ns integrant-repl-autoreload.core
+  (:require [hawk.core :as hawk]
+            [integrant.repl :as igr]))
+
+
+(defonce hawk-watcher nil)
+
+(defn- auto-reset-handler [ctx _event]
+  (binding [*ns* *ns*]
+    (integrant.repl/reset)
+    ctx))
+
+(defn- clojure-file? [_ {:keys [file]}]
+  (re-matches #"[^.].*(\.clj|\.edn)$" (.getName file)))
+
+(defn start-auto-reset
+  "Automatically reset the system when a Clojure or edn file is changed in
+  `src` or `resources`."
+  [paths]
+  (alter-var-root #'hawk-watcher
+                  (fn [watcher]
+                    (when-not (nil? watcher)
+                      (hawk/stop! watcher))
+                    (hawk/watch! [{:paths paths
+                                   :filter clojure-file?
+                                   :handler auto-reset-handler}]))))
+
+(defn stop-auto-reset
+  []
+  (alter-var-root #'hawk-watcher
+                  (fn [watcher]
+                    (when-not (nil? watcher)
+                      (hawk/stop! watcher)))))
